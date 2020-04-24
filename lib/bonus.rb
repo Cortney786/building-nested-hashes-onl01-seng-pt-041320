a1e9e8c262f7fb3439fbf218f07ec36494cb3c8a
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-24 22:23:49 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,7 @@ def bonus
    }
   }
 
- epic_tragedy[:montague[:hero][:status] = "dead" #code your solution here:
+ epic_tragedy[:montague][:hero][:status] = "dead" #code your solution here:
 
   #puts epic_tragedy
 
