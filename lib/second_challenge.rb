c8cc805bc3fe723775ce14db33d6db886e7b5dad
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-24 20:05:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,20 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriach => {},
+      :matriacrch => {},
+      :hero => {},
+      :hero_friends => []
       #your key/value pairs here!
       }, 
     :capulet => {
+      :patriach => {},
+      :heroine => {},
+      :heroine_friends => []
       #your key/value pairs here!
-      }}
+      }
+    
+  }
 
   
 end
